ee6e32298f7f7bbf1c2c85834172af7c56937630
muhaimeen90 <alamuhaimen@gmail.com>
2023-11-10 16:34:21 +0600

    Update Taskmanager.java

--- src/todolist/Taskmanager.java
+++ src/todolist/Taskmanager.java
@@ -1,5 +1,12 @@
 package todolist;
 
+import javax.sound.midi.Soundbank;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Taskmanager {
 
 	private ToDoList todoList;
@@ -13,11 +20,45 @@ public class Taskmanager {
 	}
 
 	public void saveToFile(String filename) {
-		// Implement code to save the To-Do list to a file
+		File myfile = new File(filename);
+		if(!myfile.exists()) {
+			try {
+				myfile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("Can't create file! ");
+			}
+		}
+		FileWriter fileWriter = null;
+		try {
+			fileWriter = new FileWriter(filename);
+			fileWriter.write(todoList.toString());
+			fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 	}
 
 	public void loadFromFile(String filename) {
-		// Implement code to load the To-Do list from a file
+		File myfile = new File(filename);
+		if(!myfile.exists()) {
+			System.out.println("File doesnt exist!");
+		}
+		else {
+			Scanner sc = null;
+			try {
+				sc = new Scanner(myfile);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			while(sc.hasNextLine()){
+				String line= sc.nextLine();
+				System.out.println(line);
+			}
+			sc.close();
+		}
 	}
 
 	public ToDoList getTodoList() {
